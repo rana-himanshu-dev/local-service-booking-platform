0600b34edc6933d7414c6be98e03a70437a54e3e
rana-himanshu-dev <himanshurana742006@gmail.com>
2026-06-01 10:42:40 +0530

    Added Thymeleaf fronted with landing, login, register pages

--- booking-platform/src/main/java/com/localservice/bookingplatform/config/SecurityConfig.java
+++ booking-platform/src/main/java/com/localservice/bookingplatform/config/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/support/**").authenticated()
                         .requestMatchers(HttpMethod.GET, "/api/provider/search/advanced").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/admin/reports/**").hasRole("ADMIN")
+                        .requestMatchers("/", "/login", "/register").permitAll()
                         .requestMatchers("/api/user/**").authenticated()
                         .requestMatchers("/api/provider/**").hasRole("PROVIDER")
                         .requestMatchers("/api/customer/**").hasRole("CUSTOMER")
